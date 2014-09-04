e0e1a99cd9e7879282bd0a7359a08e4be377c340
Lance Batson <lancebatsondev@gmail.com>
2014-09-03 22:21:44 -0500

    basic text field and button. update gitignore

--- iBeacons/ViewController.swift
+++ iBeacons/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var uuidTextField: UITextField!
+    @IBOutlet weak var getInfoButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        uuidTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        uuidTextField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func getInfo(sender: UIButton) {
+        uuidTextField.resignFirstResponder()
+        if (countElements(uuidTextField.text) > 0) {
+            println(uuidTextField.text)
+//            TODO: set up infoviewcontroller
+//            self.performSegueWithIdentifier("info", sender: self)
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == "info" {
+//            TODO: set data in infoviewcontroller
+        }
+    }
 }
 
